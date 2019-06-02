80fbe0d070fa6e55a575ea810dbcf9d0cc46e669
jaasonw <jayywong92@gmail.com>
2019-06-02 16:17:52 -0700

    move this into its own function

--- include/SQL.h
+++ include/SQL.h
@@ -22,34 +22,34 @@ void SQL::interactive() {
     while (true) {
         std::cout << "sql> ";
         getline(std::cin, command);
-        try {
-            auto parse_tree = parser.parse(command);
-            #ifdef DEBUG
-                std::cout << parse_tree << '\n';
-            #endif
-            // select
-            if (parse_tree["command"][0] == "select") {
-                Table t(parse_tree["table_name"][0]);
-                t.select(parse_tree["fields"], parse_tree["where"]);
-                Table temp("temp");
-                std::cout << temp << '\n';
-            }
-            // create
-            if (parse_tree["command"][0] == "create") {
-                Table t(parse_tree["table_name"][0], parse_tree["fields"]);
-            }
-            // insert
-            if (parse_tree["command"][0] == "insert") {
-                Table t(parse_tree["table_name"][0]);
-                t.insert_into(parse_tree["fields"]);
-            }
-
-        } catch (std::runtime_error e) {
-            std::cerr << e.what() << '\n';
-        }
+        execute_string(command);
     }
 }
 
-void SQL::execute_string(std::string str) {
-    
+void SQL::execute_string(std::string command) {
+    try {
+        auto parse_tree = parser.parse(command);
+        #ifdef DEBUG
+            std::cout << parse_tree << '\n';
+        #endif
+        // select
+        if (parse_tree["command"][0] == "select") {
+            Table t(parse_tree["table_name"][0]);
+            t.select(parse_tree["fields"], parse_tree["where"]);
+            Table temp("temp");
+            std::cout << temp << '\n';
+        }
+        // create
+        if (parse_tree["command"][0] == "create") {
+            Table t(parse_tree["table_name"][0], parse_tree["fields"]);
+        }
+        // insert
+        if (parse_tree["command"][0] == "insert") {
+            Table t(parse_tree["table_name"][0]);
+            t.insert_into(parse_tree["fields"]);
+        }
+
+    } catch (std::runtime_error e) {
+        std::cerr << e.what() << '\n';
+    }
 }
